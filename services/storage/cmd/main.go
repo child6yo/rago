d486e0777e430ab0eb6e6b50c5cc69e390d9c4b9
Arseniy Glushkov <miguelprod8@gmail.com>
2025-05-25 19:38:57 +0700

    renamed

---
+++ services/storage/cmd/main.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+	// llm, err := ollama.New(ollama.WithModel("qwen3:0.6b"))
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// embedder, err := embeddings.NewEmbedder(llm)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// conn := qdrant.NewQdrantConnection(embedder)
+
+	// ctx := context.Background()
+	// s, err := conn.Store.AddDocuments(ctx, []schema.Document{
+	// 	{PageContent: "Протокол SSL был изначально разработан в компании Netscape для обеспечения безопасности электронной коммерции в Вебе"},
+	// 	{PageContent: "Во время TLS-рукопожатия протокол также позволяет обеим сторонам проверить свою идентичность. В браузере этот механизм позволяет клиенту убедиться,"},
+	// 	{PageContent: "кошки и собаки - хорошие домашние животные"},
+	// 	{PageContent: "женщины - это зло"},
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println(s)
+
+	// a, _ := conn.Store.SimilaritySearch(ctx, "милота", 2)
+	// log.Println(a)
+	// a, _ = conn.Store.SimilaritySearch(ctx, "сетевой протокол", 2)
+	// log.Println(a)
+	// a, _ = conn.Store.SimilaritySearch(ctx, "что такое TLS-рукопожатие", 2)
+	// log.Println(a)
+}
