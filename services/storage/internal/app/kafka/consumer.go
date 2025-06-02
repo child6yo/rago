92a30ee5684a99dbe21e99b381a2a17637d5c4eb
Arseniy Glushkov <miguelprod8@gmail.com>
2025-06-03 01:52:17 +0700

    fix: consumer

--- services/storage/internal/app/kafka/consumer.go
+++ services/storage/internal/app/kafka/consumer.go
@@ -13,20 +13,17 @@ type ConsumerGroupHandler struct {
 }
 
 func (c ConsumerGroupHandler) Setup(session sarama.ConsumerGroupSession) error {
-	log.Println("Setting up consumer group session")
 	return nil
 }
 
 func (c ConsumerGroupHandler) Cleanup(session sarama.ConsumerGroupSession) error {
-	log.Println("Cleaning up consumer group session")
 	return nil
 }
 
 func (c ConsumerGroupHandler) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
 	for msg := range claim.Messages() {
-		log.Print(msg.Partition, msg.Topic)
 		if err := c.handler.HandleDocMessage(msg.Value); err != nil {
-			log.Printf("Ошибка обработки сообщения: %v", err)
+			log.Printf("failed to handle message: %v", err)
 			continue
 		}
 
