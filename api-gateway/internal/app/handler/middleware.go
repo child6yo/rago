9dcf5eae7285a66e2a02c09ae8218924393fa964
Arseniy Glushkov <miguelprod8@gmail.com>
2025-06-11 19:26:24 +0700

    temp: error handling

--- api-gateway/internal/app/handler/middleware.go
+++ api-gateway/internal/app/handler/middleware.go
@@ -12,14 +12,14 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	header := c.GetHeader("Authorization")
 	if header == "" {
 		log.Print("empty authorization header")
-		c.JSON(500, nil)
+		c.AbortWithStatusJSON(500, nil)
 		return
 	}
 
 	headerParts := strings.Split(header, " ")
 	if len(headerParts) != 2 || headerParts[0] != "Bearer" {
 		log.Print("invalid authorizaton header")
-		c.JSON(500, nil)
+		c.AbortWithStatusJSON(500, nil)
 		return
 	}
 
@@ -32,7 +32,7 @@ func (h *Handler) userIdentity(c *gin.Context) {
 	userId, err := h.grpclient.User.Auth(headerParts[1])
 	if err != nil {
 		log.Print("invalid token")
-		c.JSON(500, nil)
+		c.AbortWithStatusJSON(500, nil)
 		return
 	}
 
@@ -43,14 +43,14 @@ func (h *Handler) checkAPIKey(c *gin.Context) {
 	key := c.Query("api-key")
 	if key == "" {
 		log.Print("empty api key")
-		c.JSON(500, nil)
+		c.AbortWithStatusJSON(500, nil)
 		return
 	}
 
 	err := h.grpclient.User.CheckAPIKey(key)
 	if err != nil {
 		log.Print("invalid api key: ", err)
-		c.JSON(500, nil)
+		c.AbortWithStatusJSON(500, nil)
 		return
 	}
 }
