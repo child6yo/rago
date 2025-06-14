0d7c414ec150986ff573181f9dd5a30c04aced09
Арсений Глушков <arseniyglsh@gmail.com>
2025-06-15 03:06:20 +0700

    feat: embedding mock

---
+++ services/storage/internal/pkg/embedding/mock/mock.go
@@ -0,0 +1,15 @@
+package mock
+
+import "context"
+
+type Embedder struct {
+	GenerateEmbeddingsFunc func(ctx context.Context, input string) ([]float32, error)
+}
+
+// GenerateEmbeddings mock
+func (e *Embedder) GenerateEmbeddings(ctx context.Context, input string) ([]float32, error) {
+	if e.GenerateEmbeddingsFunc != nil {
+		return e.GenerateEmbeddingsFunc(ctx, input)
+	}
+	return []float32{}, nil
+}
