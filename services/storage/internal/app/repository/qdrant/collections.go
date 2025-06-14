7f8994644d1f6f25e867981df8a0083b72eb5d96
Арсений Глушков <arseniyglsh@gmail.com>
2025-06-15 03:43:00 +0700

    feat: implement collections

--- services/storage/internal/app/repository/qdrant/collections.go
+++ services/storage/internal/app/repository/qdrant/collections.go
@@ -1,13 +1,34 @@
 package qdrantrepo
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	"github.com/qdrant/go-client/qdrant"
+)
 
 // CreateCollection создает новую коллекцию.
-func (c *Client) CreateCollection(ctx context.Context, name string) error {
-	return nil
+func (c *Client) CreateCollection(ctx context.Context, name string) (err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("repository: %w", err)
+		}
+	}()
+	return c.client.CreateCollection(ctx, &qdrant.CreateCollection{
+		CollectionName: "name",
+		VectorsConfig: qdrant.NewVectorsConfig(&qdrant.VectorParams{
+			Size:     768,
+			Distance: qdrant.Distance_Cosine,
+		}),
+	})
 }
 
 // DeleteCollection удаляет коллекцию вместе с ее содержимым.
-func (c *Client) DeleteCollection(ctx context.Context, collection string) error {
-	return nil
+func (c *Client) DeleteCollection(ctx context.Context, collection string) (err error) {
+	defer func() {
+		if err != nil {
+			err = fmt.Errorf("repository: %w", err)
+		}
+	}()
+	return c.client.DeleteCollection(ctx, collection)
 }
