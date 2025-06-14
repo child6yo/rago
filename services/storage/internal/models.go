301048d30d3b50eb22e9c8863f494fd1a8a0a87e
Арсений Глушков <arseniyglsh@gmail.com>
2025-06-15 03:06:39 +0700

    feat: beautify

--- services/storage/internal/models.go
+++ services/storage/internal/models.go
@@ -8,7 +8,7 @@ type Metadata struct {
 // Document структура, определяющая объект, который необходимо
 // содержать в векторной базе данных.
 type Document struct {
-	Content  string   `json:"content"` // содержание
+	Content  string   `json:"content"`  // содержание
 	Metadata Metadata `json:"metadata"` // метаданные
-	Score float32 // используется при запросах (queries), показывает схожесть документа с пришедшим в запросе
+	Score    float32  // используется при запросах (queries), показывает схожесть документа с пришедшим в запросе
 }
