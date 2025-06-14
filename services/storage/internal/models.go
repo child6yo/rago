1da2ccd226587133ffd87734b5b543da45e18d02
Арсений Глушков <arseniyglsh@gmail.com>
2025-06-15 03:17:53 +0700

    feat: docs embeddings field

--- services/storage/internal/models.go
+++ services/storage/internal/models.go
@@ -8,7 +8,8 @@ type Metadata struct {
 // Document структура, определяющая объект, который необходимо
 // содержать в векторной базе данных.
 type Document struct {
-	Content  string   `json:"content"`  // содержание
-	Metadata Metadata `json:"metadata"` // метаданные
-	Score    float32  // используется при запросах (queries), показывает схожесть документа с пришедшим в запросе
+	Content   string    `json:"content"`  // содержание
+	Metadata  Metadata  `json:"metadata"` // метаданные
+	Embedding []float32 // векторное представление
+	Score     float32   // используется при запросах (queries), показывает схожесть документа с пришедшим в запросе
 }
